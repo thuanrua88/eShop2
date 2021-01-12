38202501361d00e246f5bcc1d6ce1c40ca65a070
thuanrua88 <thuannvth1909002@fpt.edu.vn>
2021-01-12 08:27:01 +0700

    update

---
+++ SportStore/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SportStore.Models;
+
+namespace SportStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository repository;
+        public int PageSize = 4;
+
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
+        //public IActionResult index() => View(repository.Products);
+
+        public ViewResult Index(int productPage = 1)
+            => View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize)
+                );
+
+
+        //public IActionResult Index() => View();
+    }
+}
